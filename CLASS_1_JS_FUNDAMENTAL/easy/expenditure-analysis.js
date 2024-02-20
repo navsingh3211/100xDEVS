58bc262f527ecc2b2f73509d0949bb7b9bb1576f
navsingh3211 <navsingh3211@gmail.com>
2024-02-20 17:04:10 +0530

    aolving basic esay leavel class-1 question

--- CLASS_1_JS_FUNDAMENTAL/easy/expenditure-analysis.js
+++ CLASS_1_JS_FUNDAMENTAL/easy/expenditure-analysis.js
@@ -3,7 +3,7 @@
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
   transactions is an array where each
   Transaction - an object like 
-        {
+  {
 		id: 1,
 		timestamp: 1656076800000,
 		price: 10,
@@ -12,9 +12,54 @@
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
-
+const transaction = [
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	},
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 20,
+		category: 'Food',
+		itemName: 'Burger',
+	},
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 11,
+		category: 'Fruit',
+		itemName: 'Apple',
+	},
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 22,
+		category: 'Fruit',
+		itemName: 'Mango',
+	}
+]
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+  let result = {}
+  // adding data inside the set
+  let mySet = new Set();
+  for(let i =0 ;i < transactions.length;i++){
+    if(mySet.has(transactions[i].category)){
+      result[transactions[i].category] += transactions[i].price;
+    }else{
+      result[transactions[i].category] = transactions[i].price;
+      mySet.add(transactions[i].category)
+    }
+  }
 
-module.exports = calculateTotalSpentByCategory;
+  let finalResult = Object.keys(result).map((data)=>{
+    return {category:data,price:result[data]}
+  })
+  return finalResult;
+}
+let response = calculateTotalSpentByCategory(transaction);
+console.log(response);
+// module.exports = calculateTotalSpentByCategory;
--- CLASS_1_JS_FUNDAMENTAL/easy/findLargestElement.js
+++ CLASS_1_JS_FUNDAMENTAL/easy/findLargestElement.js
@@ -6,7 +6,14 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let max = numbers[0];
+    for(let ele of numbers){
+      if(ele>max){
+        max = ele;
+      }
+    }
+    return max;
 }
-
-module.exports = findLargestElement;
+const result = findLargestElement([-1,-4,-6,-3,-9]);
+console.log(result,'result');
+// module.exports = findLargestElement;
