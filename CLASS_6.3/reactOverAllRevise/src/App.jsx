8fc94fc4976a31f79557bf10bd25df889d9098f7
navsingh3211 <navsingh3211@gmail.com>
2024-03-14 14:46:25 +0530

    assignment

--- CLASS_6.3/reactOverAllRevise/src/App.jsx
+++ CLASS_6.3/reactOverAllRevise/src/App.jsx
@@ -1,70 +1,21 @@
-/* eslint-disable react/display-name */
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
+import {useEffect, useRef, useState} from 'react';
 
-import { useEffect, useMemo, useState ,memo,useCallback} from 'react'
-import './App.css'
-
-function App() {
-  const [exchangeData1,setExchange1Data] = useState({});
-  const [exchangeData2,setExchange2Data] = useState({});
-  const [bankData,setbankData] = useState({});
-  console.log("re-rednering++");
+function App(){
+  const [incomeTax,setIncomeTax] = useState(20000);
+  const divRef = useRef();
 
   useEffect(()=>{
     setTimeout(()=>{
-      setExchange1Data({returns:100});
-    });
+      // console.log(divRef.current);
+      divRef.current.innerHTML = 10;
+    },5000);
   },[]);
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      setExchange2Data({returns:100});
-    });
-  },[]);
-
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      setbankData({income:100});
-    },2000);
-  },[]);
-
-  //useCallback is not about minimizing the amount of code that is run
-  //useCallback is about ,not re-rendering a child components,
-  // if the function hasn't/doesnt need to change across renders
-
-  const calCulateCryptoReturn = useCallback(function(){
-    return exchangeData1.returns + exchangeData2.returns;
-  },[exchangeData1,exchangeData2]);
-
-  const inComeTax = (bankData.income + calCulateCryptoReturn()) * 0.3;
 
   return (
     <div>
-      <Child calCulateCryptoReturn={calCulateCryptoReturn}/>
-      <Dummy />
+      hi there,your income tax returns are <div ref={divRef}>{incomeTax}</div>
     </div>
   )
 }
 
-const Dummy = memo(
-  function(){
-    console.log('dummy');
-    return <div>
-      hi
-    </div>
-  }
-);
-
-
-const Child = memo(
-  function Child({calCulateCryptoReturn}){
-    console.log('crypto child re-render');
-    return <div>
-      Your crypoto return are {calCulateCryptoReturn()}
-    </div>
-  }
-);
-
-export default App
+export default App;
