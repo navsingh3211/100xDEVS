3dfbd337e215e8a1adfb8fd783e7a8cf5c9c7757
navsingh3211 <navsingh3211@gmail.com>
2024-03-16 18:25:46 +0530

    context api

--- CLASS_7.1/props-drilling/src/App.jsx
+++ CLASS_7.1/props-drilling/src/App.jsx
@@ -1,15 +1,46 @@
-
-
-import './App.css'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import {CountContext} from "./context";
+import { useContext } from "react";
 
 function App() {
 
-
+  const [count,setCount] = useState(0);
   return (
-    <>
-      hi drilling
-    </>
+    <div>
+      {/* wrap anyone that wants to use the teleported value inside a provider */}
+      <CountContext.Provider value={count}>
+        <Count count={count} setCount={setCount}/>
+      </CountContext.Provider> 
+    </div>
   )
+}
+
+function Count({setCount}){
+  return <div>
+    <CountRenderer/>
+    <Buttons setCount={setCount}/>
+  </div>
+}
+
+function CountRenderer(){
+  const count = useContext(CountContext);
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}){
+  const count = useContext(CountContext);
+  return <div>
+    <button onClick={()=>{
+      setCount(count+1)
+    }}>Increase</button>
+
+    <button onClick={()=>{
+      setCount(count-1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
