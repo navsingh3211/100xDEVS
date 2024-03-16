9574cc145e2b0004fe8c886b62233203f7e87b71
navsingh3211 <navsingh3211@gmail.com>
2024-03-16 16:32:11 +0530

    routing

--- CLASS_7.1/react-routers-pro/src/App.jsx
+++ CLASS_7.1/react-routers-pro/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {lazy,Suspense} from 'react';
+import {BrowserRouter,Route,Routes, useNavigate} from 'react-router-dom';
+// import { Landing } from "./components/Landing";
+// import { Dashboard } from "./components/Dashboard";
+const Landing = lazy(()=>import("./components/Landing"));
+const Dashboard = lazy(()=>import("./components/Dashboard"));
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      
+      <BrowserRouter>
+        <Appbar/>
+        <Routes>
+          <Route path='/dashboard' element={<Suspense fallback={"loading..."}><Dashboard/></Suspense>}/>
+          <Route path='/' element={<Suspense fallback={"loading..."}><Landing/></Suspense>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
+}
+
+function Appbar(){
+  const navigate = useNavigate();
+  return <div>
+    <div style={{background:"black" , color:"white"}}>
+
+      <h1>TOp bar is good</h1>
+      <button onClick={()=>{
+        navigate("/")
+      }}>Landing</button> 
+
+      <button onClick={()=>{
+        navigate("/dashboard")
+      }}>Dashboard</button>
+
+      {/* <button onClick={()=>{
+        window.location.href = "/"
+      }}>Landing</button> 
+
+      <button onClick={()=>{
+        window.location.href = "/dashboard"
+      }}>Dashboard</button> */}
+
+      </div>
+    </div>
 }
 
 export default App
